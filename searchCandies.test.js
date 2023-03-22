93f1736b1e70d68a92bd1adc918eeb760d066f35
Valeria Serci <balerty84@gmail.com>
2023-03-22 14:44:48 +0000

    test drive shows one candy(checking price too)

--- searchCandies.test.js
+++ searchCandies.test.js
@@ -4,4 +4,8 @@ describe('searchCandies', () => {
   it('shows 2 candies', () => {
     expect(searchCandies('Ma', 10)).toEqual(['Mars', 'Maltesers']);
   });
+
+  it('shows one candy', () => {
+    expect(searchCandies('Ma', 2)).toEqual(['Mars']);
+  });
 })
