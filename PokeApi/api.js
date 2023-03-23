829f3081e9f7e57b14326461a21477c4a9cd9174
Valeria Serci <balerty84@gmail.com>
2023-03-23 19:16:27 +0000

    hitting the PokeApi challenge

---
+++ PokeApi/api.js
@@ -0,0 +1,9 @@
+const fetchPokemon = (name) => {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+
+}
+
+
+module.exports = fetchPokemon;
