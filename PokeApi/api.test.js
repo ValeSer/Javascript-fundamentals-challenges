67d6afe8ab48c49d9d4dfa789200321ca61be22d
Valeria Serci <balerty84@gmail.com>
2023-03-24 17:07:33 +0000

    complete test for fetchPokemon

--- PokeApi/api.test.js
+++ PokeApi/api.test.js
@@ -4,5 +4,9 @@ describe('fetchPokemon', () => {
   it('returns a promise of the useful pokemon data', async () => {
     const pokemon = await fetchPokemon('pikachu');
     expect(pokemon.name).toEqual('pikachu'); 
+    expect(pokemon.id).toEqual(25); 
+    expect(pokemon.height).toEqual(4);
+    expect(pokemon.weight).toEqual(60);
+    expect(pokemon.types).toEqual(['electric']);
   });
 });
