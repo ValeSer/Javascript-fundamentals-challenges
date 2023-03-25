82089f0b1630728c0ce864e79a5ef0163226b83f
Valeria Serci <balerty84@gmail.com>
2023-03-25 22:02:25 +0000

    implement fetchWeatherData

---
+++ Weather/weatherClient.js
@@ -0,0 +1,12 @@
+const apiKey = require('./apiKey');
+
+class WeatherClient {
+  constructor() {
+
+  }
+  fetchWeatherData(city) {
+    const apiUrl = `http://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}`;
+    return fetch(apiUrl)
+      .then((response) => response.json()); //converts the response from json to ann object
+  }
+}
